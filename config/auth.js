699bf9415bbd3c6fe4fda0d43c289ebb11978a9b
Mary Le <maaaryle@gmail.com>
2014-11-13 03:28:33 -0800

    redirect login to index - broken layout after

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '91715044175-9554o9u6nuo0r38hp0j321o94vevshgt.apps.googleusercontent.com',
         'clientSecret'  : 'F3buGPzDNHSAtv0WacAQaIPl',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://localhost:9999/auth/google/callback'
     }
 
 };
--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://heroku:Jd_G_BLJODfogAfIi3hMv5sCXqj51SpEXtcKvJ0WgSoIik_SySJqx_zfRzF5yYZ2xA-FvyNKv51BLK-6fmcAQA@kahana.mongohq.com:10004/app29477223'
+    //'url' : 'mongodb://heroku:Jd_G_BLJODfogAfIi3hMv5sCXqj51SpEXtcKvJ0WgSoIik_SySJqx_zfRzF5yYZ2xA-FvyNKv51BLK-6fmcAQA@kahana.mongohq.com:10004/app29477223'
+    'url' : 'mongodb://weebly:weebly@dogen.mongohq.com:10020/app31563958'
     // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
