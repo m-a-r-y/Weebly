b0bae4a59c47f4b172416fa6c40f66ff6f3845af
Mary Le <maaaryle@gmail.com>
2014-11-12 22:16:31 -0800

    auth with google

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '91715044175-9554o9u6nuo0r38hp0j321o94vevshgt.apps.googleusercontent.com',
+        'clientSecret'  : 'F3buGPzDNHSAtv0WacAQaIPl',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
